5d501c76e79199e050396b50a28ce44eae5d6b70
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-03-20 19:26:39 +0530

    git add db

--- code-server-app/database/database.js
+++ code-server-app/database/database.js
@@ -11,11 +11,11 @@ const db = mysql.createConnection({
   // database: process.env.MYSQL_ADDON_DB
 
 
-  host: process.env.MYSQL_ADDON_HOST,
-  port: process.env.MYSQL_ADDON_PORT,
-  user: process.env.MYSQL_ADDON_USER,
-  password: process.env.MYSQL_ADDON_PASSWORD,
-  database: process.env.MYSQL_ADDON_DB
+  host: 'bkwj9cpu8ofdux5nf9ms-mysql.services.clever-cloud.com',
+  port: 3306,
+  user: 'ufc52qldp1lup1jf',
+  password:'zkWnHmnOtcVaVzvyEDaB',
+  database: 'bkwj9cpu8ofdux5nf9ms'
 });
 
 
