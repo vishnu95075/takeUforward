ccfadc4c87a2b0d75c6b52d516ab3ebb895cd2fd
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-03-20 19:21:06 +0530

    add vercel

--- code-server-app/database/database.js
+++ code-server-app/database/database.js
@@ -4,6 +4,13 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" })
 
 const db = mysql.createConnection({
+  // host: process.env.MYSQL_ADDON_HOST,
+  // port: process.env.MYSQL_ADDON_PORT,
+  // user: process.env.MYSQL_ADDON_USER,
+  // password: process.env.MYSQL_ADDON_PASSWORD,
+  // database: process.env.MYSQL_ADDON_DB
+
+
   host: process.env.MYSQL_ADDON_HOST,
   port: process.env.MYSQL_ADDON_PORT,
   user: process.env.MYSQL_ADDON_USER,
