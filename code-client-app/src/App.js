f00145bf598376fa2e82695e1fe490876cd799ee
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-03-21 12:04:01 +0530

    add

--- code-client-app/src/App.js
+++ code-client-app/src/App.js
@@ -1,16 +1,16 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/Home';
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route exact path='dashboard' element={<Dashboard/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='dashboard' element={<Dashboard />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
