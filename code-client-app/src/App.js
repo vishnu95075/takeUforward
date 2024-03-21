d0c43d4afbc6fb59e811539d848915a646a074cd
Vishnu Kumar Prajapati <vishnukp95075@gmail.com>
2024-03-21 11:00:36 +0530

    add

--- code-client-app/src/App.js
+++ code-client-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path='/' Component={Home} />
-        <Route exact path='/dashboard' Component={Dashbord} />
+        <Route path='/dashboard' Component={Dashbord} />
       </Routes>
     </Router>
   );
